0d7190dfe0e3106e826ac862e9fc755f28fea544
Chad Paradis <cparadis6191@gmail.com>
2021-05-18 13:52:44 -0400

    Change read_bits_cmd.py to print bits

--- read_bits_cmd.py
+++ read_bits_cmd.py
@@ -27,8 +27,9 @@ if __name__ == '__main__':
     hex_string = ''.join(sys.stdin.readlines()).rstrip().replace(
             '\n', '').replace(' ', '')
 
-    print(hex(read_bits_from_bytearray(
+    print(format(read_bits_from_bytearray(
         binascii.unhexlify(hex_string),
         parsed_args.first_byte_index,
         parsed_args.first_bit_index,
-        parsed_args.bit_count)))
+        parsed_args.bit_count),
+        '0' + str(parsed_args.bit_count) + 'b'))
